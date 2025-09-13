09817d5519b677e15d751106bf0a72fd85f630d8
Geyzha <biantcahyo@gmail.com>
2025-09-13 12:51:28 +0700

    250441100100_Geyzha Fabbian

---
+++ 250441100100_Geyzha Fabbian/Pelcod.py
@@ -0,0 +1,47 @@
+print('='*50)
+print("jikoo")
+print("="*50)
+
+nama = "jiko" 
+print(nama)
+print(type(nama))
+
+boolean = True 
+print(type(boolean))
+
+penjumlahan = 5+5
+print("hasilnya adalah",penjumlahan)
+
+nama_asprak = "jikoo gedang"
+nama_calon_dosen = nama_asprak
+print(nama_calon_dosen)
+print("="*50)
+
+
+#tipe data 
+
+# tahun_lahir = int(input("masukan tahun lahir :"))
+# tahun_sekarang = int(input("masukan tahun sekarang :"))
+# c = tahun_sekarang - tahun_lahir
+# print("usia kamu" , c)
+
+# bil_1 = 51
+# bil_2 = 7
+
+# modulus = bil_1 % bil_2
+# print(modulus)
+
+# jumlah = int(bil_1)+int(bil_2)
+# print(jumlah)
+
+# cek = bil_1 > bil_2
+# print(cek) 
+
+saldo_awal = float(input("masukan saldio awal :"))
+deposit = float(input("masukan saldo deposit :"))
+
+
+saldo_awal += deposit
+print("saldo kamu saat ini", saldo_awal)
+
+
